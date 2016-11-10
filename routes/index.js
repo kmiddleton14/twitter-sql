997704d6a4af6c68398a8f76c31915c39a2618fc
Kathryn Middleton <kmiddleton14@gmail.com>
2016-11-10 17:17:32 -0500

    added post for db

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 var tweetBank = require('../tweetBank');
 var pg = require('../db/index');
-
+var ID = 1;;
 
 
 module.exports = function makeRouterWithSockets (io) {
@@ -61,7 +61,21 @@ module.exports = function makeRouterWithSockets (io) {
   // create a new tweet
   router.post('/tweets', function(req, res, next){
    // var newTweet = tweetBank.add(req.body.name, req.body.content);
-    pg.query('INSERT INTO tweets (userId, content) VALUES ($1, $2)', [req.body.name, req.body.content], function (err, data) {
+   //get the ID from req.body.name 
+   //var id = SELECT id from Users where name='Tom Hanks')
+   //vr ID = undefined;
+
+
+   pg.query('SELECT id from Users where name=$1', [req.body.name], function(err, result){
+      if(err) return next(err);
+      ID = Number(result.rows);
+
+      
+   });
+   console.log(ID);
+
+    pg.query('INSERT INTO tweets (userId, content) VALUES ($1, $2)', [ID, req.body.content], function (err, data) {
+        console.log("TEST");
         if(err) return next(err);
         var newTweet = result.rows;
         io.sockets.emit('new_tweet', newTweet);
@@ -69,6 +83,7 @@ module.exports = function makeRouterWithSockets (io) {
 
 
     });
+
   });
 
   // // replaced this hard-coded route with general static routing in app.js
